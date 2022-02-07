96aa99457ce7c5861a941606f9dcf9cb7592b712
Cameron Copland <cjcopland@gmail.com>
2022-02-07 15:02:19 +0000

    add get one operation

--- src/main/java/com/camcop/outfitmixer/rest/ItemController.java
+++ src/main/java/com/camcop/outfitmixer/rest/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -28,6 +29,11 @@ public class ItemController {
     @GetMapping
     public ResponseEntity<List> getAll() {
         return new ResponseEntity<List>(this.service.getAll(), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getById(@PathVariable("id") Long id, @RequestBody Item item) {
+        return new ResponseEntity<Item>(this.service.getById(id), HttpStatus.ACCEPTED);
     }
 
 
