6f0d7f1bde7bb3527b36e56b62c8c5b6cc0675e8
Cameron Copland <cjcopland@gmail.com>
2022-02-07 10:56:50 +0000

    initialise spring project

---
+++ src/main/java/com/camcop/outfitmixer/OutfitMixerApplication.java
@@ -0,0 +1,13 @@
+package com.camcop.outfitmixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfitMixerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutfitMixerApplication.class, args);
+    }
+
+}
