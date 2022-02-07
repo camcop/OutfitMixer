305e1e8046e800aea9e567f08ad22c26d8bd3d54
Cameron Copland <cjcopland@gmail.com>
2022-02-07 14:55:58 +0000

    add service and constructor

--- src/main/java/com/camcop/outfitmixer/rest/ItemController.java
+++ src/main/java/com/camcop/outfitmixer/rest/ItemController.java
@@ -1,4 +1,15 @@
 package com.camcop.outfitmixer.rest;
 
+import com.camcop.outfitmixer.service.ItemService;
+
 public class ItemController {
+
+    private final ItemService service;
+
+    public ItemController(ItemService service) {
+        super();
+        this.service = service;
+    }
+
+
 }
