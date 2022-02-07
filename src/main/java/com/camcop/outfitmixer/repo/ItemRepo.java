c7a7b24b2a0d8363fe1fa47366d069a656cd025d
Cameron Copland <cjcopland@gmail.com>
2022-02-07 12:15:04 +0000

    extend jparepository

--- src/main/java/com/camcop/outfitmixer/repo/ItemRepo.java
+++ src/main/java/com/camcop/outfitmixer/repo/ItemRepo.java
@@ -1,4 +1,12 @@
 package com.camcop.outfitmixer.repo;
 
-public interface ItemRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepo extends JpaRepository {
+
+
+
+
 }
