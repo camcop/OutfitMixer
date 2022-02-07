867ee950ba8414bdaef15ee900f44145f840819e
Cameron Copland <cjcopland@gmail.com>
2022-02-07 11:49:27 +0000

    add id field to item class

--- src/main/java/com/camcop/outfitmixer/domain/Item.java
+++ src/main/java/com/camcop/outfitmixer/domain/Item.java
@@ -1,4 +1,15 @@
 package com.camcop.outfitmixer.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Item")
 public class Item {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
