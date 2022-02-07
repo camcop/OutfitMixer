5fb03198c96aa67911e7f8795788bd66e16018a0
Cameron Copland <cjcopland@gmail.com>
2022-02-07 12:38:48 +0000

    create read all  method

--- src/main/java/com/camcop/outfitmixer/service/ServiceInterface.java
+++ src/main/java/com/camcop/outfitmixer/service/ServiceInterface.java
@@ -1,8 +1,13 @@
 package com.camcop.outfitmixer.service;
 
+import java.util.List;
+
 public interface ServiceInterface<T> {
 
     //	Create
     T add(T t);
+
+    //	Read all
+    List<T> getAll();
 
 }
