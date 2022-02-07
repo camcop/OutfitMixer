23cba04206f6a90369935f7ed93f23261f229d1a
Cameron Copland <cjcopland@gmail.com>
2022-02-07 12:00:17 +0000

    add constructors and getters and setters

--- src/main/java/com/camcop/outfitmixer/domain/Item.java
+++ src/main/java/com/camcop/outfitmixer/domain/Item.java
@@ -25,4 +25,63 @@ public class Item {
     @Column
     private String colour;
 
+    public Item() {
+    }
+
+    public Item(Long id, String name, String type, String brand, String colour) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.brand = brand;
+        this.colour = colour;
+    }
+
+    public Item(String name, String type, String brand, String colour) {
+        this.name = name;
+        this.type = type;
+        this.brand = brand;
+        this.colour = colour;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+
 }
