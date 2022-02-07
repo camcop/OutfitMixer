930bda69df4a8579e70c601f02c4a50f2dc8cd21
Cameron Copland <cjcopland@gmail.com>
2022-02-07 15:04:42 +0000

    add update operation

--- src/main/java/com/camcop/outfitmixer/rest/ItemController.java
+++ src/main/java/com/camcop/outfitmixer/rest/ItemController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -32,9 +31,13 @@ public class ItemController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Item> getById(@PathVariable("id") Long id, @RequestBody Item item) {
+    public ResponseEntity<Item> getById(@PathVariable("id") Long id) {
         return new ResponseEntity<Item>(this.service.getById(id), HttpStatus.ACCEPTED);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> updateById(@PathVariable("id") Long id, @RequestBody Item item) {
+        return new ResponseEntity<Item>(this.service.updateById(id, item), HttpStatus.ACCEPTED);
+    }
 
 }
