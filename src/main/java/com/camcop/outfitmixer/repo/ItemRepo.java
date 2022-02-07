203382006d285ebe6fae47def2eb80dc936a9db6
Cameron Copland <cjcopland@gmail.com>
2022-02-07 12:55:13 +0000

    add CRUD methods

--- src/main/java/com/camcop/outfitmixer/repo/ItemRepo.java
+++ src/main/java/com/camcop/outfitmixer/repo/ItemRepo.java
@@ -1,12 +1,11 @@
 package com.camcop.outfitmixer.repo;
 
+import com.camcop.outfitmixer.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepo extends JpaRepository {
-
-
+public interface ItemRepo extends JpaRepository<Item, Long> {
 
 
 }
--- src/main/java/com/camcop/outfitmixer/service/ItemService.java
+++ src/main/java/com/camcop/outfitmixer/service/ItemService.java
@@ -1,12 +1,50 @@
 package com.camcop.outfitmixer.service;
 
 import com.camcop.outfitmixer.domain.Item;
+import com.camcop.outfitmixer.repo.ItemRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService implements ServiceInterface<Item> {
 
+    private final ItemRepo repo;
 
+    public ItemService(ItemRepo repo) {
+        super();
+        this.repo = repo;
+    }
 
+    @Override
+    public Item add(Item item) {
+        return this.repo.save(item);
+    }
 
+    @Override
+    public List<Item> getAll() {
+        return this.repo.findAll();
+    }
+
+    @Override
+    public Item getById(Long id) {
+        return this.repo.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Item updateById(Long id, Item item) {
+        Item oldItem = this.repo.findById(id).orElseThrow();
+        oldItem.setName(item.getName());
+        oldItem.setType(item.getType());
+        oldItem.setBrand(item.getBrand());
+        oldItem.setColour(item.getColour());
+        return this.repo.save(oldItem);
+    }
+
+    @Override
+    public Item deleteById(Long id) {
+        Item deletedItem = this.repo.findById(id).orElseThrow();
+        this.repo.deleteById(id);
+        return deletedItem;
+    }
 }
