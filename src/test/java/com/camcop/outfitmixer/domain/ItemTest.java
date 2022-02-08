824bd3c44e8a61faad70b5c5e573eca4b0a15bfd
Cameron Copland <cjcopland@gmail.com>
2022-02-08 10:58:21 +0000

    add constructor tests

--- src/test/java/com/camcop/outfitmixer/domain/ItemTest.java
+++ src/test/java/com/camcop/outfitmixer/domain/ItemTest.java
@@ -1,4 +1,32 @@
 package com.camcop.outfitmixer.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class ItemTest {
+
+    private Item newItem;
+
+    @BeforeEach
+    public void setUp() {
+        newItem = new Item(1L,"Plain white t-shirt","Top", "Uniqlo", "White");
+    }
+
+    @Test
+    public void testConstructorId() {
+        assertEquals(1L, newItem.getId());
+    }
+
+    @Test
+    public void testConstructorName() {
+        assertEquals("Plain white t-shirt", newItem.getName());
+    }
+
+
 }
