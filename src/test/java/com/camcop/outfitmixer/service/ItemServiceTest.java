8207c86cd32080fc5e56f588f6c0e02efa74cb6a
Cameron Copland <cjcopland@gmail.com>
2022-02-08 15:50:05 +0000

    create add test

--- src/test/java/com/camcop/outfitmixer/service/ItemServiceTest.java
+++ src/test/java/com/camcop/outfitmixer/service/ItemServiceTest.java
@@ -3,10 +3,16 @@ package com.camcop.outfitmixer.service;
 import com.camcop.outfitmixer.domain.Item;
 import com.camcop.outfitmixer.repo.ItemRepo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,5 +33,11 @@ public class ItemServiceTest {
         savedItem = new Item(1L,"Plain white t-shirt","Top", "Uniqlo", "White");
     }
 
+    @Test
+    public void testAdd() {
+        Mockito.when(this.repo.save(newItem)).thenReturn(savedItem);
+        assertEquals(savedItem, this.service.add(newItem));
+        Mockito.verify(this.repo, Mockito.times(1)).save(newItem);
+    }
 
 }
